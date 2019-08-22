3713907d57b436c0328d8b6ca8d5e59724de2533
Unknown <genaralskar1@gmail.com>
2019-08-22 13:12:49 -0600

    Add basic GameObject pooling. Add test projectile prefab to test pooling

---
+++ Unity/Crimson Sands/Assets/Scripts/Object Pooling/PoolObjects.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// This is placed once in the scene and is used to tell all the pooled objects to spawn their objects during Awake
+/// </summary>
+public class PoolObjects : MonoBehaviour
+{
+    void Awake()
+    {
+        GameObjectPool.PoolObjects();
+    }
+
+}
