35b93671d7dc791c34a8f163dcac76c93c6e540d
genaralskar <genaralskar1@gmail.com>
2019-08-21 21:58:10 -0600

    Add start of weapons script, test weapons animations, Collin test scene

---
+++ Unity/Crimson Sands/Assets/Scripts/Weapons System/Weapon.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public int damage = 10;
+    public Transform firePoint;
+    public Animator anims;
+
+    private bool isFiring;
+
+    public bool IsFiring
+    {
+        get { return isFiring; }
+        set
+        {
+            isFiring = value;
+            SetAnimsFiring(value);
+        }
+    }
+
+    private void SetAnimsFiring(bool value)
+    {
+        anims.SetBool("IsFiring", value);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !IsFiring)
+        {
+            IsFiring = true;
+        }
+
+        if (Input.GetMouseButtonUp(0) && IsFiring)
+        {
+            IsFiring = false;
+        }
+    }
+
+    public void FireParticle()
+    {
+        
+    }
+}
