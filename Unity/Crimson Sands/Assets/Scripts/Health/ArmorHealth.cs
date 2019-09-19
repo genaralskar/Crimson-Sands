dce665298eff8c35a1d897a5a90d7dbba6a18986
genaralskar <genaralskar1@gmail.com>
2019-09-18 22:09:49 -0600

    Projectiles can damage armor and armor destroys it's joint when "dead"
    weapons also only fire with left mouse when they are marked as isPlayer
    Projectiles also spawn hit sparks when colliding with hurtbox

--- Unity/Crimson Sands/Assets/Scripts/Health/ArmorHealth.cs
+++ Unity/Crimson Sands/Assets/Scripts/Health/ArmorHealth.cs
@@ -4,8 +4,47 @@ using UnityEngine;
 
 public class ArmorHealth : Health
 {
+    [Tooltip("Rigidbody for the armor")]
+    [SerializeField]
+    private Rigidbody rb;
+
+    [Tooltip("The joint the rigidbody is connected to")]
+    [SerializeField]
+    private Joint joint;
+
+    [SerializeField]
+    private Vector3 lauchDirection = Vector3.forward;
+
+    [SerializeField]
+    private float launchForce = 100f;
+
+    private bool dead = false;
+    
     protected override void Death()
     {
-        //detach armor from car
+        if (!dead)
+        {
+            //detach armor from car
+            Destroy(joint);
+        
+            //lauch armor
+            Vector3 dir = transform.TransformDirection(lauchDirection);
+            rb.AddForce((dir * launchForce) + (Vector3.up * 50));
+        
+            //Disable hitbox?
+            DisableAllHurtboxes();
+            dead = true;
+        }
+       
+    }
+
+    protected override void OnDamage(int amount)
+    {
+        Debug.Log("Damage: " + amount);
+    }
+
+    private void CheckArmorState()
+    {
+        //checks which state the armor should be in based on health
     }
 }
