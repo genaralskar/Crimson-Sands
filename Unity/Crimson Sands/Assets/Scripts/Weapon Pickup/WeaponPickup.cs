9ef48299b398cd042c79842096f695e0276ed737
genaralskar <genaralskar1@gmail.com>
2019-09-27 13:09:51 -0600

    Add weapon pickup scripts and prefab

---
+++ Unity/Crimson Sands/Assets/Scripts/Weapon Pickup/WeaponPickup.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// this goes on the collider object for a weapon pickup
+/// <para>The collider must be set to trigger</para>
+/// </summary>
+public class WeaponPickup : MonoBehaviour
+{
+    [SerializeField] private Transform weaponSpawnPoint;
+    [SerializeField] private WeaponInfo weapon;
+
+    private void Awake()
+    {
+        //spawn weapon
+        GameObject newWeapon = Instantiate(weapon.weaponPrefab, weaponSpawnPoint);
+        newWeapon.transform.localPosition = Vector3.zero;
+        newWeapon.transform.localRotation = Quaternion.identity;
+        newWeapon.transform.localScale = Vector3.one;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        WeaponPickupHandler carWeapons = other.GetComponent<WeaponPickupHandler>();
+        carWeapons.SwapWeapons(weapon);
+        
+        OnPickedUp();
+    }
+
+    private void OnPickedUp()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Unity/Crimson Sands/Assets/Scripts/Weapon Pickup/WeaponPickupHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// This goes on the object on the vehicle that has the collider
+/// </summary>
+public class WeaponPickupHandler : MonoBehaviour
+{
+    public List<WeaponMount> weaponMounts;
+
+    public void SwapWeapons(WeaponInfo weapon)
+    {
+        foreach (var mount in weaponMounts)
+        {
+            mount.SwapWeaponHandler(weapon);
+        }
+    }
+}
