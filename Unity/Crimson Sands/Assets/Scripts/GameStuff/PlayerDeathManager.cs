e2937ee8196f20d3eb41acfe81f9659b1ff4ad49
genaralskar <genaralskar1@gmail.com>
2020-01-15 13:35:50 -0700

    add player deth handling

---
+++ Unity/Crimson Sands/Assets/Scripts/GameStuff/PlayerDeathManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerDeathManager : MonoBehaviour
+{
+    public static UnityAction PlayerDeath;
+
+    public CarHealth playerHealth;
+    
+    private void PlayerDeathHandler()
+    {
+        StopAllCoroutines();
+        if (!playerHealth) return;
+        StartCoroutine(Dead());
+    }
+    
+    private IEnumerator Dead()
+    {
+        playerHealth.gameObject.SetActive(false);
+        yield return new WaitForSeconds(2f);
+        
+        playerHealth.gameObject.SetActive(true);
+        playerHealth.transform.position = playerHealth.transform.position + Vector3.up;
+        playerHealth.SetHealth(100);
+
+    }
+}
