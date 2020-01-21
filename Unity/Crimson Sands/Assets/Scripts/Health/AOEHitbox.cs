7177373c608b8fca9d2e97f10bdfad4be4d08250
Collin <genaralskar1@gmail.com>
2020-01-21 15:09:37 -0700

    fix grenade damage, rename some objects for clarification

--- Unity/Crimson Sands/Assets/Scripts/Health/AOEHitbox.cs
+++ Unity/Crimson Sands/Assets/Scripts/Health/AOEHitbox.cs
@@ -8,20 +8,45 @@ public class AOEHitbox : Hitbox
     public float disableTime = .1f;
     public GameObjectPool explosionPool;
     public float explosiveForce = 10f;
+    public float explosionRadius = 2f;
 
     public bool damageFalloff = true;
     public AnimationCurve falloffCurve = AnimationCurve.Linear(0, 1, 1, 0);
     
     
     private int startDamage;
-    private SphereCollider col;
 
     private void Start()
     {
         startDamage = weapon.damage;
-        col = GetComponent<SphereCollider>();
+        
+        //Debug.Log($"Enabled, {transform.position}", this);
+        explosionPool.GetPooledObject(transform.position, Quaternion.identity);
+
+        //sphere overlap to see what colliders to hit
+        Collider[] cols = Physics.OverlapSphere(transform.position, explosionRadius);
+
+        //list to contained already activated weaponhits
+        List<IWeaponHit> hits = new List<IWeaponHit>();
+
+        //for each collider hit
+        foreach (var col in cols)
+        {
+            //get its weapon hit. if its null, return. if its already in the activated list, return
+            IWeaponHit wh = col.GetComponent<IWeaponHit>();
+            if (wh == null) return;
+            if (hits.Contains(wh)) return;
+            
+            SendDamage(col);
+            
+            //add to the activated list
+            hits.Add(wh);
+        }
+        
+        Invoke(nameof(Disable), disableTime);
     }
 
+    
     private void OnTriggerEnter(Collider other)
     {
         float damageMulti = 1;
@@ -53,9 +78,38 @@ public class AOEHitbox : Hitbox
 
     private void OnEnable()
     {
-        //Debug.Log($"Enabled, {transform.position}", this);
-        explosionPool.GetPooledObject(transform.position, Quaternion.identity);
-        Invoke(nameof(Disable), disableTime);
+        //startDamage = weapon.damage;
+        
+       
+    }
+
+    private void SendDamage(Collider other)
+    {
+        float damageMulti = 1;
+        IWeaponHit[] hits = other.GetComponents<IWeaponHit>();
+        foreach (var hit in hits)
+        {
+            if (damageFalloff)
+            {
+                damageMulti = DamageFalloff(other.ClosestPointOnBounds(transform.position));
+            }
+            
+            hit.OnWeaponHit(weapon, transform.position);
+            weapon.damage = startDamage;
+        }
+
+        //knockback
+        Rigidbody otherRb = other.attachedRigidbody;
+        if (!otherRb) return;
+        ArmorHealth ah = otherRb.GetComponent<ArmorHealth>();
+        
+        //if the armor health is not detached, or the armor health isPlayer is the same as the weapons isPlayer
+        if (ah && (!ah.dead || ah.isPlayer == weapon.isPlayer)) return;
+
+        if (otherRb)
+        {
+            otherRb.AddExplosionForce(explosiveForce * damageMulti, transform.position, 2f, 1);
+        }
     }
 
     private void Disable()
@@ -69,14 +123,14 @@ public class AOEHitbox : Hitbox
         float distance = Vector3.Distance(position, transform.position);
 
         //normalize distance with sizeof sphere collider
-        distance = distance / col.radius;
+        distance = distance / explosionRadius;
         
         //get damage multiplier from curve
         float damageMultiplier = falloffCurve.Evaluate(distance);
 
         float newDamage = startDamage * damageMultiplier;
         
-        //Debug.Log($"Grenade Damage = {newDamage}");
+        //Debug.Log($"Grenade Damage = {newDamage}. Weapon Damage = {weapon.damage}");
         
         weapon.damage = (int)newDamage;
 
