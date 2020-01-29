56852d1e69c953a648250c763c8bc4e81120c3cb
genaralskar <genaralskar1@gmail.com>
2020-01-29 14:43:41 -0700

    add car to Collin Final scene and add game start camera stuff

---
+++ Unity/Crimson Sands/Assets/Scripts/Game End Stuff/EndGameHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class EndGameHandler : MonoBehaviour
+{
+    public Playable endgamePlayable;
+    
+    public PlayerInput playerInput;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        //stop player inputs
+        playerInput.stopInputs = true;
+        
+        
+        //move camera out
+        
+        
+        //play end game music
+        
+        
+        //fade to black
+        
+        
+        //end screen "Crimson Sands... Will Return"
+        
+        
+    }
+}
---
+++ Unity/Crimson Sands/Assets/Scripts/GameStuff/GameStartManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Playables;
+
+public class GameStartManager : MonoBehaviour
+{
+    public Playable startGamePlayable;
+    public PlayerInput inputs;
+
+
+    public UnityAction cameraLerpDone;
+
+    public void StartGameStuff()
+    {
+        Debug.Log("Start the game!");
+        inputs.stopInputs = false;
+    }
+}
