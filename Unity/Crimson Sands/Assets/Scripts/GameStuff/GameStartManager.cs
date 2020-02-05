503902bea9e5a9ad77c5887dee7ec6231c4d648d
genaralskar <genaralskar1@gmail.com>
2020-02-05 13:54:44 -0700

    add toggle for game start cinematic

--- Unity/Crimson Sands/Assets/Scripts/GameStuff/GameStartManager.cs
+++ Unity/Crimson Sands/Assets/Scripts/GameStuff/GameStartManager.cs
@@ -6,11 +6,26 @@ using UnityEngine.Playables;
 
 public class GameStartManager : MonoBehaviour
 {
-    public Playable startGamePlayable;
+    public PlayableDirector startGamePlayable;
+    public bool playableOnStart = true;
     public PlayerInput inputs;
-
+    public Camera cineCam;
 
     public UnityAction cameraLerpDone;
+
+    private void Start()
+    {
+        if (playableOnStart)
+        {
+            inputs.stopInputs = true;
+            startGamePlayable.Play();
+        }
+        else
+        {
+            inputs.stopInputs = false;
+            cineCam.gameObject.SetActive(false);
+        }
+    }
 
     public void StartGameStuff()
     {
